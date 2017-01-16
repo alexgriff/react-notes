b419433242927928567b4c74cf9c26bf1f295012
alexgriff <alexalexgriffith@gmail.com>
2017-01-16 03:29:30 -0500

    Group Together Text into Big P tags

--- notes_frontend/src/markdown_parser/markdown_parser.js
+++ notes_frontend/src/markdown_parser/markdown_parser.js
@@ -1,42 +1,80 @@
 const SNIPPET_DELIMITER = 'ç';
+const HEADING_DELIMITER = '#';
+const IMAGE_DELIMITER = '!';
+const SNIPPET_START = '```';
 
 const markdownParser = markdown => {
   let partOfSnippet = false;
+  let partOfParagraphGroup = false;
 
   const removeUnNeeded = text => text !== '' && text[0] !== '<';
 
   const groupCodeSnippets = (accum, current) => {
-    let result = [...accum, current]
+    let result = accum
 
     if (partOfSnippet) {
-      let snippet = accum[accum.length - 1];
-      snippet += (current + '\n');
-      result = [...accum.slice(0,-1), snippet]
+      if (current.substr(0,3) !== SNIPPET_START) {
+        let snippet = accum[accum.length - 1];
+        snippet += (current + '\n');
+        result = [...accum.slice(0,-1), snippet]
+      }
     }
 
-    if (current.substr(0,3) === '```') {
+    if (current.substr(0,3) === SNIPPET_START) {
       if(!partOfSnippet) {
         partOfSnippet = true;
         result = [...accum, SNIPPET_DELIMITER]
       } else {
         partOfSnippet = false;
-        result = accum
       }
     }
 
-    return result
+    return result;
+  };
+
+  const isBodyText = text => {
+    return ![HEADING_DELIMITER, IMAGE_DELIMITER].includes(text[0]) && text.substr(0,3) !== SNIPPET_START
   }
+
+  const groupParagraphs = (accum, current) => {
+    let result = accum;
+    if(!partOfSnippet) {
+      result = [...accum, current];
+      if (isBodyText(current)) {
+        if (!partOfParagraphGroup) {
+          partOfParagraphGroup = true;
+        } else {
+          let currentGroup = accum[accum.length - 1];
+          currentGroup += ('\n\n' + current);
+          result = [...accum.slice(0,-1), currentGroup];
+        }
+      } else {
+        if (current.substr(0,3) === SNIPPET_START) {
+          result = accum;
+        }
+        partOfParagraphGroup = false;
+      }
+
+    }
+
+    return result;
+  };
+
+  const groupTogetherSections = (accum, current) => {
+    const _accum = groupCodeSnippets(accum, current);
+    return groupParagraphs(_accum, current);
+  };
 
   const mapStringToObject = text => {
     switch(text[0]) {
-      case '#':
+      case HEADING_DELIMITER:
         let arrayified = text.split(' ');
 
         return {
           element: `h${arrayified[0].length}`,
           contents: arrayified.slice(1).join(" ")
         };
-      case '!':
+      case IMAGE_DELIMITER:
         const srcRegex = /http.*(?=\))/;
         const altRegex = /\[(.*)\]/;
 
@@ -64,7 +102,7 @@ const markdownParser = markdown => {
 
   return markdown.split('\n')
     .filter(removeUnNeeded)
-    .reduce(groupCodeSnippets, [])
+    .reduce(groupTogetherSections, [])
     .map(mapStringToObject);
 }
 
